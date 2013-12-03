f58fe908c9e7dd28d91171fd600db9253b1b3be9
Ryan Richter <rrichter@gmail.com>
2013-12-02 21:45:59 -0800

    Initial Player Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,47 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	//TODO: Write this.
 
-	// Use this for initialization
-	void Start () {
-	
+	public float jumpForce = 250f;
+	public float moveForce = 200f;
+	public float maxSpeed = 2f;
+
+	private bool jump = false;
+	private Transform groundCheck;
+	private bool grounded = false;
+
+	void Awake()
+	{
+		groundCheck = transform.Find ("groundCheck");
+	}
+
+	void FixedUpdate()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+
+		rigidbody2D.AddForce(Vector2.right * inputX * moveForce);
+
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+		{
+			rigidbody2D.velocity = new Vector2(Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		}
+
+		if(jump)
+		{
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	// Test change comment
+	void Update () 
+	{
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		if(Input.GetButtonDown("Jump") && grounded)
+		{
+			jump = true;
+		}
 	}
 }
