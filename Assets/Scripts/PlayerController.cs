7a134c1397586c965f7a26e36fbbda876db48b36
Ryan Richter <rrichter@gmail.com>
2013-12-04 21:55:50 -0800

    Added basic enemy and player damage.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 
+	public int PlayerHP = 100;
 	public float jumpForce = 250f;
 	public float moveForce = 200f;
 	public float maxSpeed = 2f;
@@ -13,6 +14,9 @@ public class PlayerController : MonoBehaviour {
 	private Transform groundCheck;
 	private bool grounded = false;
 	private bool facingRight = true;
+	private bool invulnerable = false;
+	private float invulStart;
+	private bool PlayerControlEnabled = true;
 
 	void Awake()
 	{
@@ -23,30 +27,42 @@ public class PlayerController : MonoBehaviour {
 	// FixedUpdate is called once per "physics operation."
 	void FixedUpdate()
 	{
-		float inputX = Input.GetAxis("Horizontal");
-
-		rigidbody2D.AddForce(Vector2.right * inputX * moveForce);
-
-		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+		if(invulnerable)
 		{
-			rigidbody2D.velocity = new Vector2(Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+			if(invulStart + 0.5f < Time.time)
+			{
+				invulnerable = false;
+				PlayerControlEnabled = true;
+			}
 		}
 
-		if(inputX > 0 && !facingRight)
+		if(PlayerControlEnabled)
 		{
-			Flip();
-		}
-		else if(inputX < 0 && facingRight)
-		{
-			Flip();
-		}
-
-
-
-		if(jump)
-		{
-			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-			jump = false;
+			float inputX = Input.GetAxis("Horizontal");
+			
+			rigidbody2D.AddForce(Vector2.right * inputX * moveForce);
+			
+			if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+			{
+				rigidbody2D.velocity = new Vector2(Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+			}
+			
+			if(inputX > 0 && !facingRight)
+			{
+				Flip();
+			}
+			else if(inputX < 0 && facingRight)
+			{
+				Flip();
+			}
+			
+			
+			
+			if(jump)
+			{
+				rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+				jump = false;
+			}
 		}
 
 		anim.SetFloat("speed", Mathf.Abs (rigidbody2D.velocity.x));
@@ -70,5 +86,37 @@ public class PlayerController : MonoBehaviour {
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+	}
+
+	void GetHurt(int damage)
+	{
+		if(!invulnerable)
+		{
+			PlayerHP -= damage;
+			if (PlayerHP <= 0)
+			{
+				//TODO: die better
+				Destroy(gameObject);
+			}
+
+			//control is diabled and player is made invulnerable for half a second
+			PlayerControlEnabled = false;
+			invulnerable = true;
+			invulStart = Time.time;
+
+			//toggling isKinematic removes all other forces applied to the player for a cleaner knockback
+			rigidbody2D.isKinematic = true;		
+			rigidbody2D.isKinematic = false;
+
+			//knocks the player up and the opposite direction they are facing
+			if(facingRight)
+			{
+				rigidbody2D.AddForce (new Vector2(-100, 150));
+			}
+			else
+			{
+				rigidbody2D.AddForce (new Vector2(100, 150));
+			}
+		}
 	}
 }
